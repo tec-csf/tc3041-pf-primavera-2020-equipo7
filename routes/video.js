e5cf5142ddcc579624fbed53f026831aff4da72f
Emilio Cantón <eecanton@gmail.com>
2020-05-26 22:42:01 -0500

    feat: Correct authorization

--- routes/video.js
+++ routes/video.js
@@ -7,7 +7,7 @@ const router = Router();
 // POST request for creating an user
 router.post('/', authToken, videoController.postVideo);
 router.post('/:video_id', authToken, videoController.postVideoAnalysis);
-router.get('/:token', authToken, videoController.getVideos);
-router.get('/:token/:video_id', authToken, videoController.getVideo);
+router.get('/', authToken, videoController.getVideos);
+router.get('/:video_id', authToken, videoController.getVideo);
 //TODO delete
 module.exports = router;
